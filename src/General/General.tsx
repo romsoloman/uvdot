d111233f98deb8fb6e022703cdbc7fb9733cc6ba
romsolomann <rom@propdo.com>
2023-01-08 20:50:34 +0200

    feat: add general conent

--- src/General/General.tsx
+++ src/General/General.tsx
@@ -5,26 +5,23 @@ import React from "react";
 import { StyledWrapper } from "../styled/StyledWrapper";
 import theme from "../styled/theme";
 import check from "../../public/assets/icons/general/check.svg";
+import { useTranslation } from "react-i18next";
 
 export const General = () => {
+  const { t } = useTranslation();
   return (
     <StyledWrapper sx={{ backgroundColor: theme.palette.background.default }}>
       <Container maxWidth="lg">
         <Grid container justifyContent="space-between">
           <Grid container item md={4} maxHeight={100}>
             <Typography fontWeight={600} variant="h2" gutterBottom>
-              Lorem ipsum dolor
+              {t("general.title")}
             </Typography>
-            <Typography>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-              aperiam repellat neque, consequatur perferendis labore
-            </Typography>
+            <Typography>{t("general.subtitle")}</Typography>
           </Grid>
-          <Grid container item md={5}>
+          <Grid container item md={6}>
             <Grid item>
-              <Typography>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
-              </Typography>
+              <Typography>{t("general.explanation.title")} </Typography>
             </Grid>
             <Grid container spacing={2} mt={4}>
               {Array.from(Array(8), (e, i) => {
@@ -32,7 +29,7 @@ export const General = () => {
                   <Grid key={i} item md={6} display="flex">
                     <Image src={check} alt="check" width={15} height={15} />
                     <Typography variant="body2" sx={{ ml: 2 }}>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      {t(`general.explanation.caption${i + 1}`)}
                     </Typography>
                   </Grid>
                 );
